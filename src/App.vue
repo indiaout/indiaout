212f5c6ca7e5eb9c808f545b92fcf4e37c67f45a
Adnan Ahmed <dewanmohammadabdullah@gmail.com>
2024-03-24 01:42:16 +0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <hero class="w-full">
+    <div class="w-full bg-emerald-800 border-double border-red-500 border-b-8">
+      <h1 class="text-red-600 font-bold text-5xl text-center pt-20">#IndiaOut</h1>
+      <h2 class="text-green-400 font-medium text-2xl text-center">দেশি পণ্য কিনে হোন ধন্য</h2>
+      <div class="flex py-6">
+        <iframe src="https://www.youtube-nocookie.com/embed/qx8_j-i7yMc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" class="w-[90%] aspect-video rounded-xl m-auto lg:w-[70%]"></iframe>
+      </div>
+    </div>
+  </hero>
+  <main class="py-10">
+    <h2 class="text-gray-900 font-bold text-2xl text-center my-6">বাংলাদেশে <span class="text-red-600">#IndiaOut</span> ঘোষণা</h2>
+    <blockquote class="relative px-8 m-auto md:w-[80%] lg:w-[70%]">
+      <svg class="absolute -top-6 -start-8 size-16 text-gray-300 dark:text-gray-700" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path d="M7.39762 10.3C7.39762 11.0733 7.14888 11.7 6.6514 12.18C6.15392 12.6333 5.52552 12.86 4.76621 12.86C3.84979 12.86 3.09047 12.5533 2.48825 11.94C1.91222 11.3266 1.62421 10.4467 1.62421 9.29999C1.62421 8.07332 1.96459 6.87332 2.64535 5.69999C3.35231 4.49999 4.33418 3.55332 5.59098 2.85999L6.4943 4.25999C5.81354 4.73999 5.26369 5.27332 4.84476 5.85999C4.45201 6.44666 4.19017 7.12666 4.05926 7.89999C4.29491 7.79332 4.56983 7.73999 4.88403 7.73999C5.61716 7.73999 6.21938 7.97999 6.69067 8.45999C7.16197 8.93999 7.39762 9.55333 7.39762 10.3ZM14.6242 10.3C14.6242 11.0733 14.3755 11.7 13.878 12.18C13.3805 12.6333 12.7521 12.86 11.9928 12.86C11.0764 12.86 10.3171 12.5533 9.71484 11.94C9.13881 11.3266 8.85079 10.4467 8.85079 9.29999C8.85079 8.07332 9.19117 6.87332 9.87194 5.69999C10.5789 4.49999 11.5608 3.55332 12.8176 2.85999L13.7209 4.25999C13.0401 4.73999 12.4903 5.27332 12.0713 5.85999C11.6786 6.44666 11.4168 7.12666 11.2858 7.89999C11.5215 7.79332 11.7964 7.73999 12.1106 7.73999C12.8437 7.73999 13.446 7.97999 13.9173 8.45999C14.3886 8.93999 14.6242 9.55333 14.6242 10.3Z" fill="currentColor"/>
+      </svg>
+    
+      <div class="relative z-10">
+        <p class="text-gray-800 sm:text-xl dark:text-white"><em>
+          <span class="font-bold">প্রিয় নাগরিকবৃন্দ,</span><br>
+          আজ এক যুগান্তকারী সংগ্রামের সূচনা, বিজয়ের লড়াই শুরু করেছি। আমরা যেখানেই যে অবস্থাতেই থাকি না কেন, আমাদের প্রত্যেককে এই স্মরণীয় প্রচেষ্টার অবিচ্ছেদ্য অংশ হওয়ার জন্য আহ্বান জানানো হচ্ছে। অটল সংকল্প এবং স্বদেশের প্রতি গভীর ভালোবাসায় উদ্বুদ্ধ আমাদের সম্মিলিত সংকল্প আমাদের বেঁধে রাখা শিকলকে ছিন্নভিন্ন করে দিবে।<br>
+          
+          এটি কোনো গোপন অভিযান নয়; এটি একটি স্বচ্ছ যুদ্ধ যেখানে আমাদের শত্রুরা শক্তিহীন, তাদের অনিবার্য পরাজয় আমরা প্রত্যক্ষ করবে। আমরা দীর্ঘদিন ধরে হতাশা ও যন্ত্রণায় নিমজ্জিত ছিলাম। এখনই সময় জেগে ওঠার, আমাদের সর্বশক্তি দিয়ে ওদের পেছনে ছুঁড়ে ফেলে আমাদের প্রিয় বাংলাদেশের মাটি পুনরুদ্ধার করার।<br>
+          
+          আগামীকাল, আমরা আমাদের কৌশল, বিজয়ের রোডম্যাপ উন্মোচন করব। এই লড়াই সবার জন্য উন্মুক্ত, সর্বশক্তিমানের পতাকাতলে সবাইকে ঐক্যবদ্ধ হওয়ার আহ্বান। একসাথে, আমরা বিজয়ের দিকে অগ্রসর হব।<br>
+          
+          বাংলাদেশ জিন্দাবাদ!
+        </em></p>
+      </div>
+    
+      <footer class="mt-6">
+        <div class="flex items-center">
+          <div class="flex-shrink-0">
+            <img class="size-10 rounded-full ring-1 ring-red-500" src="./images/PinakiBhattacharya.jpg" alt="Image Description">
+          </div>
+          <div class="ms-4">
+            <div class="text-base font-semibold text-gray-800 dark:text-gray-400">Pinaki Bhattacharya</div>
+            <div class="text-xs text-gray-500">Activist</div>
+          </div>
+        </div>
+      </footer>
+    </blockquote>
+  </main>
+  <footer class="w-full bg-gray-900">
+    <div class="text-center py-16">
+      <h2 class="text-3xl text-red-600">#BoycottIndianProducts</h2>
+      <p class="text-xl text-green-400">ভারতীয় পণ্য নিজে বয়কট করুন এবং অন্যকে উৎসাহিত করুন</p>
+    </div>
+  </footer>
+</template>
